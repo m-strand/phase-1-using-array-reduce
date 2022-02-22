128eb682e2913d08624a300518e6d51826db925e
Brooke Strand <brooke.strand007@gmail.com>
2022-02-21 22:59:15 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+let totalBatteries = 0;
+const hereWeGo = batteryBatches.reduce(function(prev, next) {
+    return prev+next;
+}, 0);
 // Code your solution here
